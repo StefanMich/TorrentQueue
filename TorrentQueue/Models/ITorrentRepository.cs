9b6f87a1287acfc5d1c8c7d7a2d1e2f7cb3cffd5
Stefan Micheelsen <stufkan@gmail.com>
2015-02-12 08:54:15 +0100

    oh my, first commit

---
+++ TorrentQueue/Models/ITorrentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorrentQueue.Models
+{
+    interface ITorrentRepository
+    {
+        IEnumerable<Torrent> GetAll();
+        Torrent Add(Torrent t);
+        bool Update();
+    }
+}
---
+++ TorrentQueue/Models/TorrentList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TorrentQueue.Models
+{
+    public class TorrentList
+    {
+        private List<string> torrents { get; set; }
+
+        public TorrentList()
+        {
+            torrents = new List<string>();
+        }
+    }
+}
