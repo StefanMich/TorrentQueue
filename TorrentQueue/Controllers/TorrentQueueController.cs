e9672fc2f63c55f8a886561157b2753ed42a6008
deaddog <mikkelslsl@gmail.com>
2015-10-15 06:22:37 +0200

    Implemented a GET request that retrieves the top/front of the torrent queue.

--- TorrentQueue/Controllers/TorrentQueueController.cs
+++ TorrentQueue/Controllers/TorrentQueueController.cs
@@ -12,5 +12,24 @@ namespace TorrentQueue.Controllers
     {
         private static string FilePath = HttpContext.Current.Server.MapPath("~/App_Data/list.txt");
         private static object accessObj = new object();
+
+        [HttpGet, Route("top")]
+        public IHttpActionResult GetTop()
+        {
+            if (!File.Exists(FilePath))
+                return NotFound();
+
+            string link;
+            lock (accessObj)
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+                link = lines.Length > 0 ? lines[0] : null;
+            }
+
+            if (link == null)
+                return NotFound();
+            else
+                return Ok(new { link = link });
+        }
     }
 }
