302a3eb3bb343120baee3c378bd1095f02322acc
deaddog <mikkelslsl@gmail.com>
2015-10-15 06:17:05 +0200

    Created a new ApiController.

---
+++ TorrentQueue/Controllers/TorrentQueueController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TorrentQueue.Controllers
+{
+    [RoutePrefix("torrentqueue")]
+    public class TorrentQueueController : ApiController
+    {
+    }
+}
