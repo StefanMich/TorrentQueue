772d56b4993ced4acd07b04ed93494ec7391b88b
deaddog <mikkelslsl@gmail.com>
2015-10-15 06:20:22 +0200

    Created static fields in the new controller representing the path of the persistence file and a lockable object for threading.

--- TorrentQueue/Controllers/TorrentQueueController.cs
+++ TorrentQueue/Controllers/TorrentQueueController.cs
@@ -10,5 +10,7 @@ namespace TorrentQueue.Controllers
     [RoutePrefix("torrentqueue")]
     public class TorrentQueueController : ApiController
     {
+        private static string FilePath = HttpContext.Current.Server.MapPath("~/App_Data/list.txt");
+        private static object accessObj = new object();
     }
 }
