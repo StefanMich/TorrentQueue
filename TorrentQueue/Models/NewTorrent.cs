621d1ef6c72c6be0745905de4b2a04eb00ea88dd
deaddog <mikkelslsl@gmail.com>
2015-10-15 06:30:58 +0200

    Implemented the link property in NewTorrent as non-auto which throws an exception on missing link information.

--- TorrentQueue/Models/NewTorrent.cs
+++ TorrentQueue/Models/NewTorrent.cs
@@ -1,7 +1,23 @@
-﻿namespace TorrentQueue.Models
+﻿using System;
+using System.Web.Http;
+
+namespace TorrentQueue.Models
 {
     public class NewTorrent
     {
-        public string link { get; set; }
+        private string _link;
+        public string link
+        {
+            get { return _link; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+
+                // Handle formatting checks
+
+                this._link = value;
+            }
+        }
     }
 }
